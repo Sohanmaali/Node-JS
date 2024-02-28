234b7046bfc0f38246cbec82e07bcd417d9ed58c
Sohan maali <sohanmaali4@gmail.com>
2024-02-29 00:26:20 +0530

    module add

---
+++ Http-Module/index.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.end("Hello i am sohan frome indore");
+  } else if (req.url == "/about") {
+    res.end("Hello i am About Page");
+  } else if (req.url == "/contact") {
+    res.end("Hello i am Contact");
+  } else { 
+    res.writeHead(404, { "content-type": "text/html" });
+    res.end("404 Erroe Page note found");
+  }
+});
+server.listen("8000", "127.0.0.1", () => {
+  console.log("listen port 8000");
+});
+ 
