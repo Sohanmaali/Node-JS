0492b46c5e802ac086d5849c0b4920bacc62489f
Sohan maali <sohanmaali4@gmail.com>
2024-02-29 22:53:29 +0530

    file add

---
+++ Wrapper-Module/index.js
@@ -0,0 +1,10 @@
+console.log(__dirname);
+console.log(__filename);
+
+(function (exports, require, module, __filename, __dirname) {
+//   console.log("Hello Sohan");
+  var s = "sohan";
+  console.log(s);
+})();
+
+// console.log(s);
